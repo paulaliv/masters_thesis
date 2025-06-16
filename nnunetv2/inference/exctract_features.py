af162447b11782a338e85f2fb1f53ae1a8d318d4
paulaliv <paula.alfken@gmail.com>
2025-06-16 11:27:56 +0200

    modified extract_features file

--- nnunetv2/inference/exctract_features.py
+++ nnunetv2/inference/exctract_features.py
@@ -2,36 +2,20 @@ import inspect
 import itertools
 import multiprocessing
 import os
-from copy import deepcopy
-from queue import Queue
-from threading import Thread
-from time import sleep
-from typing import Tuple, Union, List, Optional
+
 
 import numpy as np
 import torch
 import SimpleITK as sitk
 
-from acvl_utils.cropping_and_padding.padding import pad_nd_image
-from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
-from batchgenerators.utilities.file_and_folder_operations import load_json, join, isfile, maybe_mkdir_p, isdir, subdirs, \
-    save_json
-from torch import nn
-from torch._dynamo import OptimizedModule
-from torch.nn.parallel import DistributedDataParallel
-from tqdm import tqdm
-
 import nnunetv2
-
+import sys
 
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
 
 
-def main():
-    input_folder = "/home/bmep/plalfken/my-scratch/STT_classification/Segmentation/nnUNetFrame/nnUNet_raw/Dataset002_SoftTissue/imagesTr"
-    output_folder = "/home/bmep/plalfken/my-scratch/STT_classification/Segmentation/nnUNetFrame/nnunet_results/Dataset002_SoftTissue/nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres/Logits_Tr"
-    model_dir = "/home/bmep/plalfken/my-scratch/STT_classification/Segmentation/nnUNetFrame/nnunet_results/Dataset002_SoftTissue/nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres"
-    folds = (0,)
+def main(input_folder, output_folder, model_dir):
+    folds = (0,1,2,3,4)
     # Create predictor
     predictor = nnUNetPredictor(
         device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
@@ -61,5 +45,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    input_folder = os.environ.get('nnUNet_raw', sys.argv[1])
+    output_folder = os.environ.get('nnUNet_results', sys.argv[2])
+    model_dir = sys.argv[3]
 
+    main(input_folder, output_folder, model_dir)
