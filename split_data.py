a3eca2602ab6cc5bd97ba191b705e871860b2e5e
paulaliv <paula.alfken@gmail.com>
2025-07-13 15:13:49 +0200

    Dice Regressor draft

--- split_data.py
+++ split_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy
 from glob import glob
 import shutil
+import json
+from sklearn.model_selection import StratifiedKFold
 
 fold_paths = {
     'fold_0': '/gpfs/home6/palfken/masters_thesis/fold_0',
@@ -19,9 +21,9 @@ output_dirs = {
     "nnunet_imagesTr": "/gpfs/home6/palfken/nnUNetFrame/nnUNet_raw/Dataset002_SoftTissue/nnunet_imagesTr/",
     "nnunet_labelsTr": "/gpfs/home6/palfken/nnUNetFrame/nnUNet_raw/Dataset002_SoftTissue/nnunet_labelsTr/",
     "QA_imagesTr": "/gpfs/home6/palfken/QA_imagesTr/",
-    "QA_labelsTr": "/gpfs/home6/palfken//QA_labelsTr/",
+    "QA_labelsTr": "/gpfs/home6/palfken/QA_labelsTr/",
     "QA_imagesTs": "/gpfs/home6/palfken/QA_imagesTs/",
-    "QA_labelsTs": "/gpfs/home6/palfken//QA_labelsTs/",
+    "QA_labelsTs": "/gpfs/home6/palfken/QA_labelsTs/",
 
 }
 
@@ -46,7 +48,27 @@ nnunet_case_ids = nnunet_ids['case_id'].unique()
 qa_train_ids = qa_train_ids['case_id'].unique()
 qa_test_ids = qa_test_ids['case_id'].unique()
 
+# Prepare arrays for splitting
+case_ids = nnunet_ids['case_id'].values
+subtypes = nnunet_ids['subtype'].values
 
+# Initialize StratifiedKFold with 5 splits, shuffle for randomness
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+splits = []
+
+for train_idx, val_idx in skf.split(case_ids, subtypes):
+    train_cases = case_ids[train_idx].tolist()
+    print(f'number of train cases: {len(train_cases)}')
+    val_cases = case_ids[val_idx].tolist()
+    print(f'number of val cases {len(val_cases)}')
+    splits.append({'train': train_cases, 'val': val_cases})
+
+# Save to JSON file
+with open('/gpfs/home6/palfken/nnUNetFrame/nnUNet_raw/Dataset002_SoftTissue/stratified_5fold_splits.json', 'w') as f:
+    json.dump(splits, f, indent=4)
+
+print("Stratified 5-fold splits saved to 'stratified_5fold_splits.json'")
 def copy_cases(case_ids, image_dst, label_dst):
     for case_id in case_ids:
 
@@ -65,7 +87,7 @@ def copy_cases(case_ids, image_dst, label_dst):
 
 
 
-# Copy files
-copy_cases(nnunet_case_ids, output_dirs["nnunet_imagesTr"], output_dirs["nnunet_labelsTr"])
-copy_cases(qa_train_ids, output_dirs["QA_imagesTr"], output_dirs["QA_labelsTr"])
-copy_cases(qa_test_ids, output_dirs["QA_imagesTs"], output_dirs["QA_labelsTs"])
+# # Copy files
+# copy_cases(nnunet_case_ids, output_dirs["nnunet_imagesTr"], output_dirs["nnunet_labelsTr"])
+# copy_cases(qa_train_ids, output_dirs["QA_imagesTr"], output_dirs["QA_labelsTr"])
+# copy_cases(qa_test_ids, output_dirs["QA_imagesTs"], output_dirs["QA_labelsTs"])
