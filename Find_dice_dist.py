349dd83fd6b6c317ea2e7565f6e2d324d3fb8f4d
paulaliv <paula.alfken@gmail.com>
2025-07-16 20:22:08 +0200

    Dice Regressor draft

---
+++ Find_dice_dist.py
@@ -0,0 +1,18 @@
+import numpy
+import pandas as pd
+
+dir_5 ="/gpfs/home6/palfken/Dice_scores_5epochs.csv"
+dir_20 = "/gpfs/home6/palfken/Dice_scores_20epochs.csv"
+
+
+df_5 = pd.read_csv(dir_5)
+df_20 = pd.read_csv(dir_20)
+
+df_20 = df_20.rename(columns={"dice_5": "dice_20"})
+
+merged_df = pd.merge(df1, df2, on="case_id", suffixes=('_df1', '_df2'))
+
+# Save or inspect
+merged_df.to_csv("/gpfs/home6/palfken/Dice_scores_all.csv", index=False)
+print(merged_df.head())
+
