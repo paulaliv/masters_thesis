feecba63fcc00f6fb540bbe3ea50bd1a6aa6a6e6
paulaliv <paula.alfken@gmail.com>
2025-07-25 15:35:19 +0200

    Dice Regressor draft

--- dice_regressor_ordinal.py
+++ dice_regressor_ordinal.py
@@ -365,8 +365,8 @@ def train_one_fold(fold,data_dir, df, splits, uncertainty_metric,plot_dir, devic
 
     class_names = ["Fail (0-0.1)", "Poor (0.1-0.5)", "Moderate(0.5-0.7)", " Good (>0.7)"]
 
-    for epoch in range(60):
-        print(f"Epoch {epoch + 1}/{60}")
+    for epoch in range(20):
+        print(f"Epoch {epoch + 1}/{20}")
         running_loss, correct, total = 0.0, 0, 0
 
         model.train()
