0e2b6c57041f009a60fe9172a4cee48f367ada04
paulaliv <paula.alfken@gmail.com>
2025-08-05 21:17:57 +0200

    added model with umap+ mask and one with umap +mask +image

--- dice_regressor_ordinal_mask.py
+++ dice_regressor_ordinal_mask.py
@@ -263,23 +263,23 @@ class CORNLoss(nn.Module):
 
 class Light3DEncoder(nn.Module):
     def __init__(self):
-        # super().__init__()
-        # # self.encoder = nn.Sequential(
-        # #     nn.Conv3d(1, 16, kernel_size=3, padding=1),
-        # #     nn.BatchNorm3d(16),
-        # #     nn.ReLU(),
-        # #     nn.MaxPool3d(2),  # halves each dimension
-        # #
-        # #     nn.Conv3d(16, 32, kernel_size=3, padding=1),
-        # #     nn.BatchNorm3d(32),
-        # #     nn.ReLU(),
-        # #     nn.MaxPool3d(2),
-        # #
-        # #     nn.Conv3d(32, 64, kernel_size=3, padding=1),
-        # #     nn.BatchNorm3d(64),
-        # #     nn.ReLU(),
-        # #     nn.AdaptiveAvgPool3d((1, 1, 1)),  # outputs [B, 64, 1, 1, 1]
-        # # )
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv3d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            nn.MaxPool3d(2),  # halves each dimension
+
+            nn.Conv3d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            nn.MaxPool3d(2),
+
+            nn.Conv3d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool3d((1, 1, 1)),  # outputs [B, 64, 1, 1, 1]
+        )
 
         # self.encoder = nn.Sequential(
         #     nn.Conv3d(1, 16, 3, padding=1), nn.BatchNorm3d(16), nn.ReLU(),
