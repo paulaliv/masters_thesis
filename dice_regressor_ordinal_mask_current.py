ffe27938952d25eca3b01e237d166efc89a17524
paulaliv <paula.alfken@gmail.com>
2025-08-08 23:46:12 +0200

    added model with umap+ mask and one with umap +mask +image

--- dice_regressor_ordinal_mask_current.py
+++ dice_regressor_ordinal_mask_current.py
@@ -648,9 +648,9 @@ def train_one_fold(fold,data_dir, df, splits, uncertainty_metric,plot_dir, devic
                 # print(f'Label shape {label.shape}')
                 preds = model(image, uncertainty)  # shape: [B, 3]
                 # print(f'model Output Shape : {preds.shape}')
-                targets = encode_ordinal_targets(label).to(preds.device)
+                #targets = encode_ordinal_targets(label).to(preds.device)
                 #print(f'Tagets shape: {targets.shape}')
-                loss = criterion(preds, targets)
+                loss = criterion(preds, label)
 
 
             scaler.scale(loss).backward()
@@ -701,10 +701,10 @@ def train_one_fold(fold,data_dir, df, splits, uncertainty_metric,plot_dir, devic
                 total_masks += image.size(0)
 
                 preds = model(image, uncertainty)
-                targets = encode_ordinal_targets(label).to(preds.device)
+                #targets = encode_ordinal_targets(label).to(preds.device)
 
 
-                loss = criterion(preds, targets)
+                loss = criterion(preds, label)
                 val_running_loss += loss.item() * image.size(0)
 
 
