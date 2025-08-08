796ef060712021c38218166fbda162ddb60dc2cf
paulaliv <paula.alfken@gmail.com>
2025-08-08 23:52:12 +0200

    added model with umap+ mask and one with umap +mask +image

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -516,7 +516,7 @@ class nnUNetPredictor(object):
                 while not proceed:
                     sleep(0.1)
                     proceed = not check_workers_alive_and_busy(export_pool, worker_list, r, allowed_num_queued=2)
-
+                self.return_features = False
                 print(f"[DEBUG] return_features is {self.return_features} (type: {type(self.return_features)})")
 
                 if self.return_features:
