bf07a1862b6388b3725bd59759d1410ed5082529
paulaliv <paula.alfken@gmail.com>
2025-08-10 18:11:04 +0200

    added model with umap+ mask and one with umap +mask +image

--- dice_regressor_ordinal_mask_current.py
+++ dice_regressor_ordinal_mask_current.py
@@ -622,8 +622,8 @@ def train_one_fold(fold,data_dir, df, splits, uncertainty_metric,plot_dir, devic
 
     class_names = ["Fail (0-0.1)", "Poor (0.1-0.5)", "Moderate(0.5-0.7)", " Good (>0.7)"]
 
-    for epoch in range(40):
-        print(f"Epoch {epoch + 1}/{40}")
+    for epoch in range(50):
+        print(f"Epoch {epoch + 1}/{50}")
         running_loss, correct, total = 0.0, 0, 0
 
         model.train()
