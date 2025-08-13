972c90b911ea023a051df41cbc4b974b9ebda149
paulaliv <paula.alfken@gmail.com>
2025-08-13 22:49:42 +0200

    added model with umap+ mask and one with umap +mask +image

--- dice_regressor_ordinal_mask_current.py
+++ dice_regressor_ordinal_mask_current.py
@@ -1392,5 +1392,5 @@ if __name__ == '__main__':
     plot_dir = sys.argv[3]
 
     #main(preprocessed, plot_dir, splits, df)
-    visualize_features(preprocessed, ood_dir, splits, df,"mutual_info", plot_dir)
+    visualize_features(preprocessed, ood_dir, splits, df,"epkl", plot_dir)
 
