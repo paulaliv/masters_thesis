5b88dbeda24e95faee4356300ebf757c3b346d16
paulaliv <paula.alfken@gmail.com>
2025-08-16 14:28:12 +0200

    added model with umap+ mask and one with umap +mask +image

--- dice_regressor_ordinal.py
+++ dice_regressor_ordinal.py
@@ -725,12 +725,12 @@ def main(data_dir, plot_dir, folds,df):
     print('FUSION: LATE')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    metrics = ['confidence', 'entropy','mutual_info','epkl']
+    metrics = ['entropy','mutual_info','epkl']
 
     param_grid = {
         'lr': [1e-3, 3e-4, 1e-4],
         'batch_size': [16, 32],
-        'warmup_epochs': [3, 5, 8],
+        'warmup_epochs': [3, 5],
         'patience': [8, 10, 15],
     }
 
